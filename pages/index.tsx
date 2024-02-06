523dc3764b3edb92fa2008849babf26f76976e5c
Nico <nicolasf-n7@users.noreply.github.com>
2024-02-06 12:46:08 +0100

    removed next-contentlayer

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,6 @@ import Page from "@/components/utility/Page";
 // import Posts from "@/components/home/Posts";
 import Skills from "@/components/home/Skills";
 import Testimonials from "@/components/home/Testimonials";
-// import { allPosts } from "contentlayer/generated";
-// import { GetStaticProps } from "next";
-
-// export const getStaticProps: GetStaticProps = async () => {
-//   return {
-//     props: {
-//       allPosts: allPosts.sort(({date: dateA}: any, {date: dateB}: any) => dateB - dateA),
-//     },
-//   };
-// }
 
 export default function Home() {
   return (
